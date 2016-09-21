89a7aabccec51237f9ef49fddc4bee7100903b8b
mohseen <mohseenmukaddam6@gmail.com>
2016-09-20 23:49:18 -0700

    changed

--- src/learning/skillset.js
+++ src/learning/skillset.js
@@ -6,30 +6,30 @@
 //take the current value, max value, and update function
 'use strict';
 var Skillset = function ( params ) {
-	this._iq = params.iq || 0;
+	this._iq         = params.iq || 0;
 	this._creativity = params.creativity || 0;
-	this._charisma = params.charisma || 0;
-	this._strength = params.strength || 0;
-	this._endurance = params.endurance || 0;
+	this._charisma   = params.charisma || 0;
+	this._strength   = params.strength || 0;
+	this._endurance  = params.endurance || 0;
 	console.log( "Skillset created!!" );
 };
 Skillset.prototype = {
 	constructor: Skillset,
 	get_attr: function(){
 		return{
-			iq: this._iq,
-			creativity: this._creativity,
-			charisma: this._charisma,
-			strength: this._charisma,
-			endurance: this._endurance
+			iq         : this._iq,
+			creativity : this._creativity,
+			charisma   : this._charisma,
+			strength   : this._charisma,
+			endurance  : this._endurance
 		};
 	},
 	set_attr: function( params ){
-		this._iq = params.iq || this._iq;
+		this._iq         = params.iq || this._iq;
 		this._creativity = params.creativity || this._creativity;
-		this._charisma = params.charisma || this._charisma;
-		this._strength = params.strength || this._strength;
-		this._endurance = params.endurance || this._endurance;
+		this._charisma   = params.charisma || this._charisma;
+		this._strength   = params.strength || this._strength;
+		this._endurance  = params.endurance || this._endurance;
 	}
 };
 module.exports = Skillset;
