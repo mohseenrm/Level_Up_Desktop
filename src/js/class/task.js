f4b8cd5a1e49d97ff668c674ab0db2d2d486a4fb
mohseenrm <mohseenmukaddam6@gmail.com>
2016-10-01 01:05:26 -0700

    last commit of the day

---
+++ src/js/class/task.js
@@ -0,0 +1,27 @@
+'use strict';
+/**
+ * Task Class File
+ * Author: Mohseen Mukaddam
+ * @param {[Object]} task_list [List of all tasks to work on]
+ */
+let Task = function( params ){
+	this._task_name = params.name || "Name not provided";
+	this._task_desc = params.desc || "Description not provided";
+	this._skillset  = params.skillset || "List of skillsets to modify";
+
+}
+Task.prototype = {
+	constructor: Task,
+	get_attr: function(){
+		return{
+			name     : this._task_name,
+			desc     : this._task_desc,
+			skillset : this._skillset
+		};
+	},
+	set_attr: function( params ){
+		this._task_name = params.name || this._task_name;
+		this._task_desc = params.desc || this._task_desc;
+		this._skillset = params.skillset || this._skillset;
+	}
+};
