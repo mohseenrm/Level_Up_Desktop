998d1c04c85fe67953e225096ef4f1762c2d1035
mohseenrm <mohseenmukaddam6@gmail.com>
2016-10-26 18:32:57 -0700

    cleaning it up

--- src/js/animation/button_effect.js
+++ src/js/animation/button_effect.js
@@ -7,57 +7,57 @@ const browser_window = node_require( 'electron' ).remote.getCurrentWindow();
 
 $(function() {
 //function definitions
-  let get_user_credentials = () => {
-    let user_credentials = {
-      username : '',
-      password : ''
+    let get_user_credentials = () => {
+      let user_credentials = {
+          username : '',
+          password : ''
+      };
+      let $username = $( "#username" ), $password = $( "#password" );
+      user_credentials[ 'username' ] = $username.val();
+      user_credentials[ 'password' ] = $password.val();
+      return( user_credentials );
     };
-    let $username = $( "#username" ), $password = $( "#password" );
-    user_credentials[ 'username' ] = $username.val();
-    user_credentials[ 'password' ] = $password.val();
-    return( user_credentials );
-  };
 
 //animations
-  let $button    = $( "#button" );
-  let class_name = "invalidate";
-  let response   = false;
-  let path       = '';
+    let $button    = $( "#button" );
+    let class_name = "invalidate";
+    let response   = false;
+    let path       = '';
 
-  $button.click(function() {
-    //get user data
-    let user_credentials = get_user_credentials();
-    response = ipcRenderer.sendSync( 'auth-user', user_credentials );
+    $button.click(function() {
+      //get user data
+        let user_credentials = get_user_credentials();
+        response = ipcRenderer.sendSync( 'auth-user', user_credentials );
 
-    console.log( response );
+        console.log( response );
 
-    if( response.auth === true )
-      class_name = 'validate';
-    else
-      class_name = 'invalidate';
+        if( response.auth === true )
+            class_name = 'validate';
+        else
+            class_name = 'invalidate';
 
-    path = response.path;
+        path = response.path;
 
-    $button.addClass( "onclic", 250, validate );
-  });
+        $button.addClass( "onclic", 250, validate );
+    });
 
-  function validate() {
-    setTimeout(function() {
+    function validate() {
+        setTimeout(function() {
 
-      $button.removeClass( "onclic" );
-      $button.addClass( class_name, 450, callback );
-    }, 2250 );
-  }
-  function callback() {
-      setTimeout(function() {
-        $button.removeClass( class_name );
-        //call login method here
-        if( class_name === 'validate' ){
-          console.log( browser_window );
-          console.log( path );
-          browser_window.loadURL( 'file://' + path );
-        }
-      }, 1250 );
-  }
+            $button.removeClass( "onclic" );
+            $button.addClass( class_name, 450, callback );
 
+        }, 2250 );
+    }
+    function callback() {
+        setTimeout(function() {
+
+          $button.removeClass( class_name );
+
+          //loading the dashboard
+          if( class_name === 'validate' )
+              browser_window.loadURL( 'file://' + path );
+
+        }, 1250 );
+    }
 });
