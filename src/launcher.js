e26f94568aed665883c3363cfea0100b748065e8
mohseenrm <mohseenmukaddam6@gmail.com>
2016-11-30 23:23:49 -0700

    testing alignment

--- src/launcher.js
+++ src/launcher.js
@@ -29,8 +29,8 @@ function createWindow () {
   // win = new BrowserWindow({width: 800, height: 600})
 
   win = new BrowserWindow({
-    width          : 1024,
-    height         : 800
+    width   : 1024,
+    height  : 800
   });
 
   // and load the index.html of the app.
